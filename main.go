9db94b110a6a2703dfb6326338c21c6ec3e82fed
HP657 <junlee5070@gmail.com>
2024-07-09 09:20:40 +0900

    함수 및 반복문 ㄷㄷ

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+// func multiply(a int, b int) int {
+// 	return a * b
+// }
+
+// func lenAndUpper(name string) (int, string) {
+// 	return len(name), strings.ToUpper(name)
+// }
+
+func repeatMe(words ...string) {
+	for i := 0; i < len(words); i++ {
+		fmt.Println(words[i])
+	}
+	fmt.Println(words)
+}
 
 func main() {
+	repeatMe("hyo", "jun", "man")
+	// totalLengtht, upperName := lenAndUpper("HP657")
+	// fmt.Println(totalLengtht, upperName)
+	// fmt.Println(multiply(2, 4))
 	// name := "hyojun"
-	p := false
-	var name string = "hyojun"
-	name = "leehyojun"
-	fmt.Println(name)
-	fmt.Println(!p)
+	// p := false
+	// var name string = "hyojun"
+	// name = "leehyojun"
+	// fmt.Println(name)
+	// fmt.Println(!p)
 }
